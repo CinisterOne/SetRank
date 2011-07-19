c8144c799bee6a7aa0d55f136e082e6761da22e3
Baummann1703 <Baummann1703@gmx.de>
2011-07-19 15:55:13 +0200

    SetRank

---
+++ src/com/Baummann/SetRank/SetRank/spl.java
@@ -0,0 +1,26 @@
+package com.Baummann.SetRank.SetRank;
+
+import org.bukkit.ChatColor; 
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerListener;
+
+public class spl extends PlayerListener {
+    public static SetRank plugin;
+    
+    public spl(SetRank instance) {
+    	plugin = instance;
+    }
+    
+    public void onPlayerJoin(PlayerJoinEvent event) {
+    	Player player = event.getPlayer();
+    	String a = "";
+    	String g = plugin.getGroup(player);
+    	if (g.startsWith("a") || g.startsWith("e") || g.startsWith("i") || g.startsWith("o") || g.startsWith("u") || g.startsWith("A") || g.startsWith("E") || g.startsWith("I") || g.startsWith("O") || g.startsWith("U"))
+    		a = "an";
+    	else
+    		a = "a";
+    	if (SetRank.broadcastRankOnLogin)
+    	    event.setJoinMessage(ChatColor.DARK_RED + player.getName() + " " + ChatColor.DARK_RED + a + " " + ChatColor.YELLOW + g + ChatColor.DARK_RED + " joined the game!");
+    }
+}
